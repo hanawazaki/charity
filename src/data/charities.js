c0659206f713097398f8ef65c5b8d7942269d26d
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-12-14 09:09:06 +0700

    convert HTML to svelte component

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: 'First Charity Project',
+      category: 'Money',
+      thumbnail: 'https://www.openaccessgovernment.org/wp-content/uploads/2021/03/dreamstime_m_135369144-696x440.jpg',
+      pledged: 99000,
+      target: 100000,
+      date_end: +new Date('10 August 2022'),
+      profile_photo: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+      profile_name: 'Viroide Bueno',
+      no_pledges: 0
+    }
+  ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { charities } from "../data/charities";
+  import Header from "../components/Header.svelte";
+  import Welcome from "../components/Welcome.svelte";
+  import CharityList from "../components/CharityList.svelte";
+  import Promo from "../components/Promo.svelte";
+  import Footer from "../components/Footer.svelte";
+
+  let title = "Charity App";
+</script>
+
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
+
+<style>
+</style>
